85a03c2b50f6f4c7679450f05329d4a5278f798d
Kiril Gusakov <gusakov24@gmail.com>
2021-07-27 11:06:54 +0300

    try/catch ready to use

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -5,14 +5,18 @@ public class FirstTask {
     private Integer enteredNumber;
 
     public void consoleOut() {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        enteredNumber = in.nextInt();
-        System.out.println("Your number is: " + enteredNumber);
-        if (enteredNumber > 7) {
-            System.out.println("Hello" + "\n");
-        } else {
-            System.out.print("\n");
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter the number: ");
+            enteredNumber = in.nextInt();
+            System.out.println("Your number is: " + enteredNumber);
+            if (enteredNumber > 7) {
+                System.out.println("Hello" + "\n");
+            } else {
+                System.out.print("\n");
+            }
+        } catch (Exception e) {
+            System.out.println("Numbers is not entered");;
         }
     }
 
